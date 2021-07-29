247fee2a099a21d5958548ec8022e58cc862dea6
Guillermo Lopez <guillermolopez6988@yahoo.com>
2021-07-28 20:43:57 -0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Guillermo Lopez on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
